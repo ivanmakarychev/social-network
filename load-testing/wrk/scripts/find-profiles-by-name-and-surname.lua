55aecdee6e90990722cded35adbe0887c7e1aa21
Ivan Makarychev <imakarychev@ozon.ru>
2022-06-01 10:13:17 +0300

    task-2: load testing

---
+++ load-testing/wrk/scripts/find-profiles-by-name-and-surname.lua
@@ -0,0 +1,15 @@
+local urlEncodedCyrillicLetters = {"%D0%B0", "%D0%B1", "%D0%B2", "%D0%B3", "%D0%B4", "%D0%B5", "%D0%B6", "%D0%B7", "%D0%B8", "%D0%BA", "%D0%BB", "%D0%BC", "%D0%BD", "%D0%BE", "%D0%BF", "%D1%80", "%D1%81", "%D1%82", "%D1%83", "%D1%84", "%D1%85"}
+
+local function randomLetter ()
+   letter = urlEncodedCyrillicLetters[ math.random( #urlEncodedCyrillicLetters ) ]
+   return letter
+end
+
+math.randomseed(os.time())
+
+request = function()
+   name =  randomLetter() .. randomLetter()
+   surname = randomLetter() .. randomLetter()
+   path = string.format("/profiles?name=%s&surname=%s", name, surname)
+   return wrk.format("GET", path)
+end
